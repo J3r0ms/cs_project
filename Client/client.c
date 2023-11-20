60b40a1c60cedebb42bf80c8a7309a960077afc3
J3r0ms <72269638+J3r0ms@users.noreply.github.com>
2023-11-20 18:13:26 +0100

    Update client.c

--- Client/client.c
+++ Client/client.c
@@ -1,7 +1,7 @@
-#include <netinet/in.h> //structure for storing address information 
+#include <netinet/in.h> //structure for storing address information
 #include <stdio.h> // for printf()
 #include <stdlib.h> // for exit()
-#include <sys/socket.h> //for socket APIs 
+#include <sys/socket.h> //for socket APIs
 #include <sys/types.h> //for data types
 #include <ctype.h>
 #include <string.h>
@@ -30,12 +30,13 @@ void retrieve_Password(int sockD)
 		scanf("%200s", userPass);
 
 		send(sockD, userPass, sizeof(userPass), 0);
+		printf("Message sent \n");
 }
 
 
 void give_counter_choice(int sockD)
 {
-	int amount; 
+	int amount;
  	// Ask the user to type an id
  	printf("Enter a positive number to increase the counter or a "
 	"negative counter to decrease the counter: \n");
@@ -55,7 +56,7 @@ void give_counter_choice(int sockD)
 		send(sockD, message, sizeof(message), 0);
 		send(sockD, &amount, sizeof(amount), 0);
 	}
-	
+
 	// if choice is decrease
 	else if (amount < 0){
 		// Send message + amount to server
@@ -68,14 +69,14 @@ void give_counter_choice(int sockD)
 int give_exit_choice(int sockD){
 	int choice;
 	printf("Do you want to exit, then type '0', or continue, then type '1': \n");
-	scanf("%1i", choice);
+	scanf("%1s", choice);
 
 	while(!isdigit(choice)) // for security purposes to avoid unexpected behavior
 	{
 		printf("Please enter a valid choice: \n");
-		scanf("%1i", choice);
+		scanf("%1s", choice);
 	}
-	
+
 		if (choice == 0){
 			// Send message to exit connection with server
 			char message[255] = "exit";
@@ -86,47 +87,60 @@ int give_exit_choice(int sockD){
 }
 
 
-int main(int argc, char const* argv[]) 
-{ 
+int main(int argc, char const* argv[])
+{
 	int sockD = socket(AF_INET, SOCK_STREAM, 0); // Internal client socket
 
 	struct sockaddr_in servAddr; // Server address
 
 	servAddr.sin_family = AF_INET; // IPv4
 	servAddr.sin_port // Port number
-		= htons(9001); // use some unused port number 
+		= htons(9001); // use some unused port number
 	servAddr.sin_addr.s_addr = INADDR_ANY; // IP address
 
-	int connectStatus 
-		= connect(sockD, (struct sockaddr*)&servAddr, 
+	int connectStatus
+		= connect(sockD, (struct sockaddr*)&servAddr,
 				sizeof(servAddr)); // Connect to server
 
 	if (connectStatus == -1) { // Error handling
 		printf("Connection error \n");
-	} 
+	}
 	else { // Actions here
 
-		char strData[255]; 
+		char strData[255];
 
-		recv(sockD, strData, sizeof(strData), 0); 
+		recv(sockD, strData, sizeof(strData), 0);
 		printf("%s\n", strData);
 
 		retrieve_ID(sockD);
 
+		int server_id_answer;
+		recv(sockD, server_id_answer, sizeof(server_id_answer), 0);
+		if (server_id_answer == 1) {
+			printf("There was an error with your id, please try again \n");
+			retrieve_ID(sockD);
+		}
 		retrieve_Password(sockD);
 
-		// give_counter_choice(sockD);
+		int server_pass_answer;
+		recv(sockD, server_pass_answer, sizeof(server_pass_answer), 0);
+		if (server_pass_answer == 1) {
+			printf("There was an error with your password, please try again \n");
+			retrieve_Password(sockD);
+		}
+
+		give_counter_choice(sockD);
 
 		// Give the user a choice to exit or continue
 		while(give_exit_choice(sockD) == 1){
 			give_counter_choice(sockD);
 		}
-		
+
 		// Close the socket
 		close(sockD);
-	} 
+	}
 
-	return 0; 
+	return 0;
 }
 
 
